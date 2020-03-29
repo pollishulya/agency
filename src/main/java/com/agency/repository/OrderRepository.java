94b5c3ad2f75965c001ce58ebddf790239dbcd7a
Pollime <57046129+pollishullya@users.noreply.github.com>
2020-03-29 16:17:55 +0300

    Вот с этого коммита все должно запускаться

--- src/main/java/com/agency/repository/OrderRepository.java
+++ src/main/java/com/agency/repository/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.agency.repository;
 
-import com.agency.entity.Order;
+/*import com.agency.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +8,4 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
     List<Order> findAllByAccountId(Long id);
-}
+}*/
